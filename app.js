bc0b6e9f765130d30281d34a0214c2c12dcece5d
aliHassanaliofficial <alihassanmagix2017@gmail.com>
2024-07-21 16:42:23 +0300

    modified:   app.js

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
+// Handle 404 errors
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', 'pages', '404.html')); // Make sure you have a 404.html file in the public/pages directory
+});
+
+// Global error handler (optional)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
