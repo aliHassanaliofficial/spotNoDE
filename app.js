4fff41ad147d49a743851c1cb53d428c0734a9b0
aliHassanaliofficial <alihassanmagix2017@gmail.com>
2024-07-24 00:59:24 +0300

    modified:   app.js 	new file:   fileConfig.js 	modified:   netlify.toml 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-const fs = require('fs');
+const fs = require('fs')
+const { filePath } = require('./fileConfig');
+
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,26 +13,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/teachers/mHaggag-centers', (req, res) => {
-    const filePath = path.join(__dirname, 'public', 'MHaggag-centers.pdf');
-    
-    console.log(`Attempting to send file: ${filePath}`);
-
-    if (!fs.existsSync(filePath)) {
-        console.error(`File not found: ${filePath}`);
-        return res.status(404).send('File not found');
-    }
-
-    // Send the PDF file
-    res.sendFile(filePath, (err) => {
-        if (err) {
-            console.error(`Error sending file: ${err}`);
-            return res.status(500).send('Failed to load PDF document');
-        } else {
-            console.log(`File sent successfully: ${filePath}`);
-        }
-    });
+app.get('/teachers/HaggagFouad/centers', (req, res) => {
+    var file = fs.createReadStream('./public/MHaggag-centers.pdf');
+    var stat = fs.statSync('./public/MHaggag-centers.pdf');
+    res.setHeader('Content-Length', stat.size);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=HaggafFouad-centers.pdf');
+    file.pipe(res);
 });
+
+
 
 // Handle 404 errors
 app.use((req, res, next) => {
@@ -42,6 +34,8 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ fileConfig.js
@@ -0,0 +1,3 @@
+const path = require('path');
+const filePath = path.join(__dirname, 'public', '/public', 'MHaggag-centers.pdf');
+module.exports = { filePath };
